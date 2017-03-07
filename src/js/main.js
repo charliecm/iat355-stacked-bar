14f5cf483f53a68f8717a64c4fcb19a9babd47cb
Charlie Chao <charliechao@outlook.com>
2017-03-07 15:03:24 -0800

    Implement static chart.

--- src/js/main.js
+++ src/js/main.js
@@ -11,26 +11,30 @@ document.addEventListener('DOMContentLoaded', function() {
 		dataURL = 'AutomobileSales.csv',
 		activeDimension = '',
 		dimensions = [
-			{
-				label: 'Cars',
-				name: 'carSales',
-				color: '#ccff00'
-			},
-			{
-				label: 'Bikes',
-				name: 'bikeSales',
-				color: '#c00ff3'
-			},
-			{
-				label: 'Trucks',
-				name: 'truckSales',
-				color: '#00cf33'
-			},
-		];
+			'carSales',
+			'bikeSales',
+			'truckSales'
+		],
+		labels = d3.scaleOrdinal([
+			'Cars', 'Bikes', 'Truckers'
+		]),
+		colors = d3.scaleOrdinal([
+			'#ccff00', '#c00ff3', '#00cf33'
+		]),
+		dataMax = 0;
 
 	// UI
 	var canvas = document.getElementById('canvas'),
-		svg = d3.select(canvas).append('svg');
+		svg = d3.select(canvas).append('svg'),
+		gWrap, gXAxis, gYAxis, gBars,
+		margin = {
+			top: 40,
+			bottom: 40,
+			left: 80,
+			right: 0
+		},
+		xScale, yScale, yAxisScale,
+		xAxis, yAxis;
 
 	// https://github.com/jashkenas/underscore/blob/master/underscore.js#L880
 	function debounce(func, wait, immediate) {
@@ -71,14 +75,14 @@ document.addEventListener('DOMContentLoaded', function() {
 			eleItem = document.createElement('li');
 			eleItem.className = 'legends__item';
 			eleItem.addEventListener('click', function() {
-				activeDimension = dimension.name;
+				activeDimension = dimension;
 			});
 			eleColor = document.createElement('span');
 			eleColor.className = 'legends__color';
-			eleColor.style.backgroundColor = dimension.color;
+			eleColor.style.backgroundColor = colors(dimension);
 			eleLabel = document.createElement('span');
 			eleLabel.className = 'legends__label';
-			eleLabel.textContent = dimension.label;
+			eleLabel.textContent = labels(dimension);
 			eleItem.appendChild(eleColor);
 			eleItem.appendChild(eleLabel);
 			eleList.appendChild(eleItem);
@@ -87,28 +91,97 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Setup diagram canvas
 	function setupDiagram() {
-		// gWrap = svg.append('g');
+		gWrap = svg.append('g')
+			.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+		gBars = gWrap.append('g');
+		gXAxis = gWrap.append('g');
+		gXAxis.append('line');
+		gYAxis = gWrap.append('g');
 	}
 
 	// Draws the diagram
 	function redraw() {
-		var width = 300; // canvas.clientWidth;
+		var outerWidth = canvas.clientWidth,
+			outerHeight = 360,
+			width = outerWidth - margin.left - margin.right,
+			height = outerHeight - margin.top - margin.bottom;
+		xScale = d3.scaleBand().rangeRound([ 0, width ]).padding(0.1);
+		xAxis = d3.axisBottom().scale(xScale);
+		yScale = d3.scaleLinear().range([ height / 2, 0]);
+		yAxisScale = d3.scaleLinear().range([ height, 0 ]);
+		yAxis = d3.axisLeft().scale(yAxisScale);
+		// heightScale = d3.scale.linear().range([ 0, height / 2]);
+		xScale.domain(data.map(function(d) {
+			return d.month;
+		}));
+		yScale.domain([ 0, dataMax ]);
+		yAxisScale.domain([ -dataMax, dataMax ]);
+		var stack = d3.stack().keys([ 'carSales', 'bikeSales', 'truckSales' ]);
+		var series = stack(data);
 		// Resize
 		svg.attrs({
-			width: width,
-			height: width
+			width: outerWidth,
+			height: outerHeight
 		});
+		// x-axis
+		gXAxis.select('line')
+			.attrs({
+				x1: 0,
+				y1: height / 2 + 0.5,
+				x2: width,
+				y2: height / 2 + 0.5,
+				stroke: 'black'
+			});
+		// gXAxis.attr('transform', 'translate(0, ' + height / 2 + ')')
+		// 	.call(d3.axisBottom(dataX));
+		gYAxis.call(yAxis.ticks(10, '.2s'));
+
+		var gSeries = gBars.selectAll('.series').data(series);
+		gSeries.exit().remove();
+		gSeries = gSeries.enter()
+			.append('g')
+			.attr('class', 'series')
+			.attr('fill', function(d) {
+				return colors(d.key);
+			})
+			.merge(gSeries);
+
+		var gSeriesBars = gSeries.selectAll('rect')
+			.data(function(d) {
+				return d;
+			});
+		gSeriesBars.exit().remove();
+		gSeriesBars
+			.enter()
+				.append('rect')
+			.merge(gSeriesBars)
+				.attrs({
+					x: function(d) {
+						return xScale(d.data.month);
+					},
+					y: function(d) {
+						return yScale(d[1]);
+					},
+					width: xScale.bandwidth(),
+					height: function(d) {
+						return yScale(d[0]) - yScale(d[1]);
+					}
+				});
 	}
 
 	// Fetches and parses the data
 	function fetch() {
 		d3.csv(dataURL)
 			.row(function(d) {
+				var carSales = +d['Car Sales'],
+					bikeSales = +d['Bike Sales'],
+					truckSales = +d['Truck Sales'];
+				dataMax = Math.max(dataMax, Math.max(carSales + bikeSales + truckSales));
 				return {
 					month: d['Month'],
-					carSales: +d['Car Sales'],
-					bikeSales: +d['Bike Sales'],
-					truckSales: +d['Truck Sales']
+					carSales: carSales,
+					bikeSales: bikeSales,
+					truckSales: truckSales
 				};
 			})
 			.get(function(d) {
