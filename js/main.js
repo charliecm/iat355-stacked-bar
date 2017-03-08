64159d33ddd94f20ade559f59da8a108e2af109e
Charlie Chao <charliechao@outlook.com>
2017-03-07 17:13:16 -0800

    Tweak copy. Implement interaction.

--- js/main.js
+++ js/main.js
@@ -9,14 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	// Data
 	var data,
 		dataURL = 'AutomobileSales.csv',
-		activeDimension = '',
 		dimensions = [
 			'carSales',
 			'bikeSales',
 			'truckSales'
 		],
 		labels = d3.scaleOrdinal([
-			'Cars', 'Bikes', 'Truckers'
+			'Cars', 'Bikes', 'Trucks'
 		]),
 		colors = d3.scaleOrdinal(d3.schemeCategory10),
 		yMax = 0,
@@ -28,13 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
 		svg = d3.select(canvas).append('svg'),
 		gWrap, gXAxis, gAxisLabels, gYAxis, gBars,
 		margin = {
-			top: 40,
-			bottom: 40,
+			top: 24,
+			bottom: 64,
 			left: 80,
 			right: 0
 		},
-		xScale, yScale, yAxisScale,
-		xAxis, yAxis;
+		offsets = [],
+		offsetIndex = 0,
+		transitionDuration = 600;
 
 	// https://github.com/jashkenas/underscore/blob/master/underscore.js#L880
 	function debounce(func, wait, immediate) {
@@ -68,14 +68,19 @@ document.addEventListener('DOMContentLoaded', function() {
 		};
 	}
 
+	function updateOffset(index) {
+		offsetIndex = index;
+		redraw();
+	}
+
 	function setupLegends() {
 		var eleList = document.getElementById('legends-list'),
 			eleItem, eleColor, eleLabel;
-		dimensions.forEach(function(dimension) {
+		dimensions.forEach(function(dimension, i) {
 			eleItem = document.createElement('li');
 			eleItem.className = 'legends__item';
 			eleItem.addEventListener('click', function() {
-				activeDimension = dimension;
+				updateOffset(i);
 			});
 			eleColor = document.createElement('span');
 			eleColor.className = 'legends__color';
@@ -101,24 +106,31 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	// Draws the visualization
-	function redraw() {
+	function redraw(isInit) {
 
 		var outerWidth = canvas.clientWidth,
 			outerHeight = canvas.clientHeight,
 			width = outerWidth - margin.left - margin.right,
-			height = outerHeight - margin.top - margin.bottom;
+			height = outerHeight - margin.top - margin.bottom,
+			duration = (isInit) ? 0 : transitionDuration;
 
 		// Calculate scales
-		xScale = d3.scaleBand().rangeRound([ 0, width ]).padding(0.05);
-		xAxis = d3.axisBottom().scale(xScale);
-		yScale = d3.scaleLinear().range([ height / 2, 0]);
-		yAxisScale = d3.scaleLinear().range([ height, 0 ]);
-		yAxis = d3.axisLeft().scale(yAxisScale);
-		xScale.domain(data.map(function(d) {
-			return d.month;
-		}));
-		yScale.domain([ 0, yMax ]);
-		yAxisScale.domain([ -yMax, yMax ]);
+		var xScale = d3.scaleBand()
+				.rangeRound([ 0, width ])
+				.padding(0.05)
+				.domain(data.map(function(d) {
+					return d.month;
+				})),
+			xAxis = d3.axisBottom().scale(xScale),
+			yScale = d3.scaleLinear()
+				.range([ height / 2, 0])
+				.domain([ 0, yMax ]),
+			yAxisScale = d3.scaleLinear()
+				.range([ height, 0 ])
+				.domain([ -yMax, yMax ]),
+			yAxis = d3.axisLeft().scale(yAxisScale),
+			yOffsetScale = d3.scaleLinear().range([ 0, height / 2 ]).domain([ 0, yMax ]),
+			offsetMax = yOffsetScale(d3.max(offsets[offsetIndex]));
 
 		// Resize canvas
 		svg.attrs({
@@ -140,17 +152,15 @@ document.addEventListener('DOMContentLoaded', function() {
 		// x-axis labels
 		gAxisLabels
 			.attr('class', 'chart__x-axis')
-			.attr('transform', 'translate(0, ' + height / 2 + ')')
-			.call(xAxis);
-
-		// gXAxis.selectAll('text')
-		// 	.transition().duration(1000)
-		// 	.attr('transform', 'translate(0,' + d3.max(offsets[offset]) + ')');
+			.call(xAxis)
+			.selectAll('text')
+			.transition().duration(duration)
+				.attr('transform', 'translate(0, ' + (height / 2 + offsetMax) + ')');
 
 		// y-axis
 		gYAxis.call(yAxis.ticks(10, '.2s'));
 
-		// Populate series
+		// Series group
 		var gSeries = gBars.selectAll('.series').data(series);
 		gSeries.exit().remove();
 		gSeries = gSeries.enter()
@@ -161,51 +171,63 @@ document.addEventListener('DOMContentLoaded', function() {
 			})
 			.merge(gSeries);
 
-		// Populate bars for each series
+		// Series bars
 		var gSeriesBars = gSeries.selectAll('g')
-			.data(function(d) {
-				return d;
+			.data(function(d, i) {
+				return d.map(function(d) {
+					d.series = i;
+					return d;
+				});
 			});
 		gSeriesBars.exit().remove();
 		gSeriesBars
 			.enter()
 				.append('g')
-				.each(function() {
-					d3.select(this).append('rect');
-					d3.select(this).append('text');
+				.each(function(d, i) {
+					d.index = i;
+					d3.select(this).append('rect').datum(d)
+						.on('click', function() {
+							updateOffset(d.series);
+						});
+					d3.select(this).append('text').datum(d);
 				});
 		gSeriesBars = gSeries.selectAll('g');
 
+		// Bars
 		gSeriesBars.selectAll('rect')
 			.attrs({
+				class: 'chart__bar',
 				x: function(d) {
 					return xScale(d.data.month);
-				},
-				y: function(d) {
-					return yScale(d[1]);
 				},
 				width: xScale.bandwidth(),
 				height: function(d) {
 					return yScale(d[0]) - yScale(d[1]);
 				}
-			});
+			})
+			.transition().duration(duration)
+				.attr('y', function(d) {
+					return yScale(d[1] - offsets[offsetIndex][d.index]);
+				});
 
+		// Bar value labels
 		gSeriesBars.selectAll('text')
 			.attrs({
 				class: 'chart__value',
 				dx: function(d) {
 					return xScale(d.data.month) + xScale.bandwidth() / 2;
 				},
-				dy: function(d) {
-					return yScale(d[1]) + 2;
-				},
 				width: xScale.bandwidth(),
 				'text-anchor': 'middle',
 				'alignment-baseline': 'text-before-edge'
 			})
 			.text(function(d) {
-				return d3.format('.2s')(d[1]);
-			});
+				return d3.format('.2s')(d.data[dimensions[d.series]]);
+			})
+			.transition().duration(duration)
+				.attr('dy', function(d) {
+					return yScale(d[1] - offsets[offsetIndex][d.index]) + 3;
+				});
 
 	}
 
@@ -228,9 +250,15 @@ document.addEventListener('DOMContentLoaded', function() {
 				// Initialize visualization
 				data = d;
 				series = stack(data);
+				offsets = series.map(function(row) {
+					var offset = row.map(function(column) {
+						return column[0];
+					});
+					return offset;
+				});
 				setupLegends();
 				setupVis();
-				redraw();
+				redraw(true);
 				window.addEventListener('resize', debounce(redraw, 500));
 			});
 	}
